cb351920ec62162f0b217bd125472cbf92ff393e
fifau <fifaultimateamfifa15@gmail.com>
2023-03-17 23:46:54 +0700

    осталось: редактирование

--- js/main.js
+++ js/main.js
@@ -12,10 +12,10 @@ Vue.component('all-notes', {
 Vue.component('notes', {
     template: `
         <div class="notes">
-            <note :column="columns[0]" :errors="errors" :name="nameFirst"   :columnIndex="columnIndex1" @deleteTask="deleteTask" @nextColumn="nextColumn"       @editTask="editTask"></note>
-            <note :column="columns[1]" :errors="errors" :name="nameSecond"  :columnIndex="columnIndex2" @nextColumn="nextColumn" @clearTrues="clearTrues"></note>
-            <note :column="columns[2]" :errors="errors" :name="nameThird"   :columnIndex="columnIndex3" @nextColumn="nextColumn" @reasonBackFun="reasonBackFun" @reasonStatusEdit="reasonStatusEdit"></note>
-            <note :column="columns[3]" :errors="errors" :name="nameFourth"  :columnIndex="columnIndex4" :gg="gg" :errorGG="errorGG"></note>
+            <note class="notes-note" :column="columns[0]" :errors="errors" :name="nameFirst"   :columnIndex="columnIndex1" @deleteTask="deleteTask" @nextColumn="nextColumn"       @editTask="editTask"></note>
+            <note class="notes-note" :column="columns[1]" :errors="errors" :name="nameSecond"  :columnIndex="columnIndex2" @nextColumn="nextColumn" @clearTrues="clearTrues"></note>
+            <note class="notes-note" :column="columns[2]" :errors="errors" :name="nameThird"   :columnIndex="columnIndex3" @nextColumn="nextColumn" @reasonBackFun="reasonBackFun" @reasonStatusEdit="reasonStatusEdit"></note>
+            <note class="notes-note" :column="columns[3]" :errors="errors" :name="nameFourth"  :columnIndex="columnIndex4"></note>
         </div>
     `,
 
@@ -39,13 +39,30 @@ Vue.component('notes', {
             columnIndex2: 1,
             columnIndex3: 2,
             columnIndex4: 3,
-
-            gg: [],
-            errorGG: [],
         }
     },
 
     mounted(){
+        eventBus.$on('check-deadline', () => {
+            this.columns[3].forEach(note => {
+                if (note){
+                    let date1 = new Date(note.date)
+                    let date2 = new Date(note.deadline)
+                    console.log("date", date1, typeof (date1))
+                    console.log("deadline", date2, typeof (date2))
+                    console.log("note", note, typeof (note))
+                    console.log("errorGG", note.errorGG, typeof (note.errorGG))
+                    console.log("gg", note.gg, typeof (note.gg))
+
+                    if (date1 <= date2){
+                        note.gg.push("GG")
+                    }else{
+                        note.errorGG.push("GABELLA")
+                    }
+                }
+            })
+        })
+
         const savedColumns = localStorage.getItem('columns');
         if (savedColumns) {
             this.columns = JSON.parse(savedColumns);
@@ -61,22 +78,12 @@ Vue.component('notes', {
                 }
             });
         });
-        eventBus.$on('endColumn', () => {
-            this.columns[3].forEach(note => {
-                if (note){
-                    if (note.date <= note.deadline){
-                        this.gg.push("GG")
-                    }else{
-                        this.errorGG.push("GABELLA")
-                    }
-                }
-            })
-        })
+
     },
     watch: {
         columns: {
             handler: 'saveNotes',
-            deep: true
+            deep: true,
         },
     },
     methods: {
@@ -89,11 +96,12 @@ Vue.component('notes', {
         },
 
         nextColumn(task) {
-
-            console.log(this.columns[task.columnIndex][task.indexNote].date,'-', this.columns[task.columnIndex][task.indexNote].deadline)
-            console.log(typeof (this.columns[task.columnIndex][task.indexNote].date),'-', typeof (this.columns[task.columnIndex][task.indexNote].deadline))
             let move = this.columns[task.columnIndex].splice(task.indexNote, 1)
             this.columns[task.columnIndex+1].push(...move)
+            if (task.columnIndex+1 === 3) {
+                console.log(move[0])
+                eventBus.$emit('check-deadline', move[0]);
+            }
         },
 
         editTask(task){
@@ -113,7 +121,7 @@ Vue.component('notes', {
         },
         clearTrues(task){
             eventBus.$emit('clear-trues', task)
-        }
+        },
     }
 
 })
@@ -137,14 +145,6 @@ Vue.component('note', {
             type: Number,
             required: true
         },
-        gg: {
-            type: Array,
-            required: false
-        },
-        errorGG: {
-            type: Array,
-            required: false
-        }
     },
 
     template: `
@@ -153,24 +153,38 @@ Vue.component('note', {
             <div class="note">
                 <ul>
                     <li v-for="(note, indexNote) in column" class="li-list" >
-                        <h1>{{ gg }}</h1>
-                        <h2>{{ note.name }}</h2>
-                        <p><b>Описание: </b>{{ note.desc }}</p>
-                        <p><b>Начало: </b>{{ note.date }}</p>
-                        <p><b>Дэдлайн: </b>{{ note.deadline }}</p>
-                        <p v-if="note.reasonStatus === false"><b>reasonStatus: </b>{{ note.reasonStatus }}</p>
+                        <div class="all">
+                            
+                                <div class="errors" v-if="note.gg && columnIndex === 3">
+                                    <h1>{{ note.gg[0] }}</h1>
+                                    
+                                </div>
+<!--                                <img v-if="note.gg" class="image1" :src="image"  />-->
+                                <div class="errors" v-if="note.errorGG && columnIndex === 3">
+                                    <h1>{{ note.errorGG[0] }}</h1>
+                                </div>
+                            
+                            <div class="main">
+                                <h2>{{ note.name }}</h2>
+                                <p><b>Описание: </b>{{ note.desc }}</p>
+                                <p><b>Начало: </b>{{ note.date }}</p>
+                                <p><b>Дэдлайн: </b>{{ note.deadline }}</p>
+                                <p v-if="note.reasonStatus === false"><b>reasonStatus: </b>{{ note.reasonStatus }}</p>
+                                
+                                <p v-for="error in errors"> {{ error }}</p>
+                                <p v-if="note.reason !== null"><b>Причина: </b>{{ note.reason }}</p>
+                                <button v-show="columnIndex !== 3" @click="nextColumn(columnIndex, indexNote, name)">Далее</button>
+                                <button v-show="columnIndex === 0" @click="deleteTask(columnIndex, indexNote, name)">Удалить</button>
+                                <button v-show="columnIndex === 2" @click="reasonStatusEdit(columnIndex, indexNote, name, note)">Вернуть</button>
+                                <button v-show="columnIndex !== 3" @click="editTask(columnIndex, indexNote, name)">Редактировать</button>
+                            </div>
+                        </div>
                         
-                        <p v-for="error in errors"> {{ error }}</p>
-                        <p v-if="note.reason !== null"><b>Причина: </b>{{ note.reason }}</p>
-                        <button v-show="columnIndex === 0" @click="deleteTask(columnIndex, indexNote, name)">Удалить</button>
-                        <button v-show="columnIndex !== 3" @click="nextColumn(columnIndex, indexNote, name)">Далее</button>
-                        <button v-show="columnIndex !== 3" @click="editTask(columnIndex, indexNote, name)">Редактировать</button>
-                        <button v-show="columnIndex === 2" @click="reasonStatusEdit(columnIndex, indexNote, name, note)">Вернуть</button>
+                       
                         <form v-show="note.reasonStatus === true" @submit.prevent="reasonBackFun(reasonBack, columnIndex, indexNote)">
                             <input type="text" v-model="reasonBack" placeholder="Причина" required>
                             <input type="submit">
                         </form>
-                        
                     </li>
                 </ul>
             </div>
@@ -179,7 +193,9 @@ Vue.component('note', {
 
     data(){
         return{
-            reasonBack: null
+            reasonBack: null,
+            // gandalf: "./assets/gandalf.gif",
+            // papich: "./assets/papich.gif",
         }
     },
 
@@ -204,6 +220,12 @@ Vue.component('note', {
         },
 
     },
+
+    computed: {
+        // image() {
+        //     return this.gandalf
+        // },
+    }
 })
 
 Vue.component('create-note', {
@@ -228,8 +250,10 @@ Vue.component('create-note', {
             deadline: null,
             reasonStatus: false,
             reason: null,
-            id: 0,
             errors: [],
+            gg: [],
+            errorGG: [],
+            id: null,
         }
     },
 
@@ -243,13 +267,19 @@ Vue.component('create-note', {
                 editStatus: false,
                 reasonStatus: this.reasonStatus,
                 deadline: this.deadline,
-                reason: this.reason
+                reason: this.reason,
+                gg: this.gg,
+                errorGG: this.errorGG,
+                id: this.id
             }
             eventBus.$emit('notes-submitted', note);
             this.name = null;
             this.desc = null;
             this.date = null
             this.deadline = null
+        },
+        idUp(){
+            this.id++
         },
 
         dateAdd(){
